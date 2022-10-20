f134d0779f2231cc616e8ce2f23d9c71e3e09b72
jbofficial87 <jbofficial@outlook.in>
2022-10-20 20:55:34 +0530

    Shopping Cart feature implementation.

---
+++ src/TailwindTraders.Api.Core/Requests/Validators/UpdateCartItemQuantityRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿namespace TailwindTraders.Api.Core.Requests.Validators;
+
+public class UpdateCartItemQuantityRequestValidator : AbstractValidator<UpdateCartItemQuantityRequest>
+{
+    public UpdateCartItemQuantityRequestValidator()
+    {
+        RuleFor(x => x.CartItem.CartItemId)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("CartItemId cannot be null/empty.");
+        RuleFor(x => x.CartItem.Quantity)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Quantity cannot be null/empty.");
+    }
+}
--- src/TailwindTraders.Api.Core/Services/Interfaces/ICartService.cs
+++ src/TailwindTraders.Api.Core/Services/Interfaces/ICartService.cs
@@ -2,4 +2,35 @@
 
 internal interface ICartService
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="email"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task<List<CartDto>> GetCartAsync(string email, CancellationToken cancellationToken);
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="cartItemDto"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task AddItemToCartAsync(CartDto cartItemDto, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="cartItemDto"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task UpdateCartItemQuantityAsync(CartDto cartItemDto, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="cartItemDto"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task RemoveItemFromCartAsync(CartDto cartItemDto, CancellationToken cancellationToken);
 }
