1a65ff686977767f84a289ecc11b5d9a1483634a
jbofficial87 <jbofficial@outlook.in>
2022-11-09 15:09:14 +0530

    Unwanted model removed.

--- src/TailwindTraders.Api.Core/Models/Implementations/Dto/ImageAnalysisViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;  
-  
-namespace TailwindTraders.Api.Core.Models.Implementations.Dto
-{
-    public class ImageAnalysisViewModel
-    {
-        public ImageAnalysis imageAnalysisResult { get; set; }
-    }
-}
