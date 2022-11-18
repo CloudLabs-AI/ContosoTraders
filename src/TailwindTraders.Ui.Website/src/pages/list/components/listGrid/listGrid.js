ad47ef41630389966f8d2dee2738787bd256a7ff
gokul <gokul.sm@seqato.com>
2022-11-18 14:19:06 +0530

    latest feedback changes

--- src/TailwindTraders.Ui.Website/src/pages/list/components/listGrid/listGrid.js
+++ src/TailwindTraders.Ui.Website/src/pages/list/components/listGrid/listGrid.js
@@ -8,7 +8,7 @@ const ListGrid = ({ productsList }) => {
     return (
         <div className="list-grid">
             <div className="filter-section">
-                <div className="page">Showing 1 - 9 of 40 items</div>
+                <div className="page">Showing 1 - {productsList.length} of {productsList.length} items</div>
                 <div>Sort By</div>&nbsp;&nbsp;&nbsp;&nbsp;
                 <MinimalSelect />
             </div>
